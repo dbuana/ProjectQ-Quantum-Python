3dc092cee97c2b83e5f978cad19f83fc8bc09fc3
dbuana <46995714+dbuana@users.noreply.github.com>
2020-09-08 18:29:37 +0900

    Quantum Teleportation + Updates and bug fixes

--- quantum.py
+++ quantum.py
@@ -1,4 +1,3 @@
-# Gaol: This program has to switch quantum states: z=[x,y], output: z=[y,x].
 from projectq.ops import All, CNOT, H, Measure, X, Y
 from projectq import MainEngine
 
@@ -7,13 +6,25 @@ qubitOne = int(input(stateEngine.allocate_qubit()))
 qubitTwo = int(input(stateEngine.allocate_qubit()))
 
 def quantum_teleportation(qubitOne, qubitTwo):
+    # Gaol: This program has to switch quantum states: z=[x,y], output: z=[y,x].
+
+    # Inner Workings: With application of Hadamard's matrix, I can create a quantum program which
+    # returns a random quantum state. However, the quantum state needs switches; with quantum teleportation
     H | qubitOne
     H | qubitTwo
     H | measure
+    # The main program engine
     for stateEngine in (qubitOne and qubitTwo):
         if qubitOne[1] == 1 and qubitTwo[0] == 0:
-            return (X | qubitOne, Y | qubitTwo = Y | qubitTwo, X | qubitOne)
+            quantum_value = int(qubitOne)
+            quantum_value_two = int(qubitTwo)
+            X | quantum_value
+            Y | quantum_value_two
+            return (quantum_value and quantume_value_two)
         elif qubitOne[1] == 0 and qubitTwo[0] == 1:
-            return (Y | qubitOne, X | qubitTwo = X | qubitTwo, Y | qubitTwo)
+            X | qubitOne
+            result = int(qubitOne)
+            return result 
         else:
-            return (qubitOne and qubitTwo)
+            quantum = [qubitOne, qubitTwo]
+            return quantum
